be5b312b140a041e7f5f9f444554614f3f12e6ac
Kelvin <kelvinkonoor@gmail.com>
2025-09-29 23:29:59 +0530

    replace ? with nan

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,9 +1,19 @@
+import yaml
 import logging
+import pandas as pd
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def preprocess_data():
-    pass
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+    
+    raw_path = config['data']['raw_path']
+    logging.info(f"Loading data from {raw_path}")
+    df = pd.read_csv(raw_path)
+
+    df.replace('?', np.nan, inplace=True)
 
 if __name__ == '__main__':
     preprocess_data()
