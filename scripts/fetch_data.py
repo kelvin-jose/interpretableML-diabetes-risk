3bcec77cde1774f09de358d743bb5dd964237172
Kelvin <kelvinkonoor@gmail.com>
2025-09-29 23:21:41 +0530

    this file is responsible for downloading and saving the dataset

---
+++ scripts/fetch_data.py
@@ -0,0 +1,13 @@
+import os
+import yaml
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+def fetch_data():
+    pass
+
+if __name__ == '__main__':
+    fetch_data()
