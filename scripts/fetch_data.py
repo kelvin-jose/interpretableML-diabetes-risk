8b907a20c99a7b2b03ca066c9c64cde09dd99947
Kelvin <kelvinkonoor@gmail.com>
2025-09-29 23:22:56 +0530

    basic check to see if data exist or not

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -7,7 +7,20 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 def fetch_data():
-    pass
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    url = config['data']['dataset_url']
+    raw_path = config['data']['raw_path']
+    raw_dir = os.path.dirname(raw_path)
+
+    if not os.path.exists(raw_dir):
+        os.makedirs(raw_dir)
+        logging.info(f"Created directory: {raw_dir}")
+
+    if os.path.exists(raw_path):
+        logging.info("Data already exists. Skipping download.")
+        return
 
 if __name__ == '__main__':
     fetch_data()
