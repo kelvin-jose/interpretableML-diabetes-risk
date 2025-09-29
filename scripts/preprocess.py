d34cfc1c9e353e91dc629ddb0313a521e9545484
Kelvin <kelvinkonoor@gmail.com>
2025-09-29 23:28:14 +0530

    this file preprocesses the downloaded files

---
+++ scripts/preprocess.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def preprocess_data():
+    pass
+
+if __name__ == '__main__':
+    preprocess_data()
