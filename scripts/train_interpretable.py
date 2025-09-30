0a2babd838906fc34e1dec5436d21035905a3f94
Kelvin <kelvinkonoor@gmail.com>
2025-09-30 10:41:22 +0530

    loading train data

--- scripts/train_interpretable.py
+++ scripts/train_interpretable.py
@@ -1,9 +1,16 @@
+import yaml
 import logging
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def train_model():
-    pass
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+    
+    train_df = pd.read_csv(config['data']['processed_train_path'])
+    X_train = train_df.drop('readmitted', axis=1)
+    y_train = train_df['readmitted']
 
 if __name__ == '__main__':
     train_model()
