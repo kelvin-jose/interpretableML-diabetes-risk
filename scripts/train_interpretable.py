9ecc7d29e7d15d5ba701eab01c4841ba4e0d0f8a
Kelvin <kelvinkonoor@gmail.com>
2025-09-30 10:39:17 +0530

    interpretable model training

---
+++ scripts/train_interpretable.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def train_model():
+    pass
+
+if __name__ == '__main__':
+    train_model()
