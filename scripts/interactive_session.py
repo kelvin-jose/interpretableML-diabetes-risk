899da6b06faf0bf62e086b10154854eb34cb879b
Kelvin <kelvinkonoor@gmail.com>
2025-09-30 10:53:56 +0530

    interactive session to add constraints

---
+++ scripts/interactive_session.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def start_interactive_session():
+    pass
+
+if __name__ == '__main__':
+    start_interactive_session()
