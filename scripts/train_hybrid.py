ba7e0307b6e5da7af7cc2b837996aeb64d90ad25
Kelvin <kelvinkonoor@gmail.com>
2025-10-01 23:04:36 +0530

    loading data for model training

--- scripts/train_hybrid.py
+++ scripts/train_hybrid.py
@@ -1,9 +1,20 @@
+import re
+import yaml
 import logging
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def train_hybrid_models():
-    pass
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    # --- Load Data ---
+    logging.info("Loading processed training data.")
+    train_df = pd.read_csv(config['data']['processed_train_path'])
+    train_df = train_df.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+    X_train = train_df.drop('readmitted', axis=1)
+    y_train = train_df['readmitted']
 
 if __name__ == '__main__':
     train_hybrid_models()
