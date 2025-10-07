a27b9721e84a3bcc9c16dda40d562093098d1a77
Kelvin <kelvinkonoor@gmail.com>
2025-10-07 08:55:58 +0530

    evaluation script

---
+++ scripts/evaluate.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def evaluate_models():
+    pass
+
+if __name__ == '__main__':
+    evaluate_models()
